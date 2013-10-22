988618b57e81d8eba1606bb98b39b519b79eda89
LaiJin <LuaJin312@gmail.com>
2013-10-22 17:25:49 +0800

    init

---
+++ Podfile
@@ -0,0 +1,3 @@
+platform :ios
+pod 'OCMapper', '~> 1.0.1'
+pod 'ASIHTTPRequest', '~> 1.8.1'
---
+++ iphoneReader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iphoneReader
+//
+//  Created by xyooyy on 13-10-22.
+//  Copyright (c) 2013年 lunajin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
