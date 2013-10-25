fe42da12018f2f4977dfb8e1ed517bcd5c93b0c6
LaiJin <LuaJin312@gmail.com>
2013-10-25 21:00:36 +0800

    删除了查看Mapping日志文件的代码

--- iphoneReader/AppDelegate.m
+++ iphoneReader/AppDelegate.m
@@ -9,7 +9,6 @@
 #import "AppDelegate.h"
 #import "RootViewController.h"
 #import "ObjectInstanceProvider.h"
-#import "CommonLoggingProvider.h"
 #import "ObjectMapper.h"
 #import "Book.h"
 
@@ -18,17 +17,12 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     ObjectInstanceProvider *instanceProvider = [[ObjectInstanceProvider alloc] init];
 	self.inCodeMappingProvider = [[InCodeMappingProvider alloc] init];
-    CommonLoggingProvider *commonLoggingProvider = [[CommonLoggingProvider alloc] initWithLogLevel:LogLevelInfo];//用于检测网页上哪些key值没有被Mapping
-    [[ObjectMapper sharedInstance] setLoggingProvider:commonLoggingProvider];
-    [self.inCodeMappingProvider mapFromDictionaryKey:@"id" toPropertyKey:@"book_id"
-                                            forClass:[Book class]];//如果设置book的属性值和网页上的key不同的时候，
-                                                                   //可以用写这句代码让它识别。
 	[[ObjectMapper sharedInstance] setInstanceProvider:instanceProvider];
 	[[ObjectMapper sharedInstance] setMappingProvider:self.inCodeMappingProvider];
+    [self.inCodeMappingProvider mapFromDictionaryKey:@"id" toPropertyKey:@"book_id"forClass:[Book class]];//如果设置book的属性值和网页上的key不同的时候，可以用写这句代码让它识别。
     RootViewController *rootViewController = [[RootViewController alloc]init];
     self.window.rootViewController = rootViewController;
     [self.window makeKeyAndVisible];
